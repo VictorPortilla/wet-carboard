79b7e696aa04fb6e6cd49be97dc2df9678aed848
ShushoRiv <73494815+A01655181@users.noreply.github.com>
2024-07-30 14:34:56 -0600

    Banshee V1
    The first version of the banshee. Complete with animations and the fade in/out effect.

--- scripts/enemies/banshee/banshee.gd
+++ scripts/enemies/banshee/banshee.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var speed : float = 300
+var isScreaming : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,12 +13,20 @@ func _process(delta):
 	
 	position.x -= speed * delta
 	
-	if position.x == get_viewport_rect().size.x / 2:
+	if position.x < 1280 and position.x > get_viewport_rect().size.x / 2:
+		$AnimatedSprite2D.self_modulate.a += 0.7 * delta
+	
+	
+	if position.x < get_viewport_rect().size.x / 2 and !isScreaming:
+		isScreaming = true
 		$AnimatedSprite2D.play("Scream")
 	
 	if position.x < 0:
 		queue_free()
 	
 func _set_start_position():
+	$AnimatedSprite2D.self_modulate.a = 0
+	$AnimatedSprite2D.play("Fly")
+	isScreaming = false
 	position.x = get_viewport_rect().size.x + 10
 	position.y = 200
