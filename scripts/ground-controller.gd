f403cc37dd3ea8c24d05cee1e90fb3538f7d0afb
coolestKitten <sepcaro2@gmail.com>
2024-07-31 19:27:21 -0600

    Limited how far left inactive ground instances can move. Added an additional ground type

--- scripts/ground-controller.gd
+++ scripts/ground-controller.gd
@@ -4,20 +4,19 @@ var groundBasic = preload("res://prefabs/ground-basic.tscn")
 var groundOneSpike = preload("res://prefabs/ground-onespike.tscn")
 var groundTwoSpikes = preload("res://prefabs/ground-twospikes.tscn")
 var groundPit = preload("res://prefabs/ground-pit.tscn")
+var groundPitOneSpike = preload("res://prefabs/ground-pit-onespike.tscn")
 
-var groundTypes = [groundBasic, groundOneSpike, groundTwoSpikes, groundPit]
+var groundTypes = [groundBasic, groundOneSpike, groundTwoSpikes, groundPit, groundPitOneSpike]
 var groundInstances : Array
 
-var SPEED = 25.0
+var SPEED = 20.0
 var screenSize : Vector2i
-var lastGroundCalled 
 
 var activeGround : Node
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Holaaaa")
 	screenSize = Vector2i(1920,1080)
 	_generate_ground_instances()
 	activeGround = groundInstances[0]
@@ -33,17 +32,18 @@ func _physics_process(_delta):
 	pass
 
 func _generate_ground_instances():
-	for i in range (4): 
+	for i in range (5): 
 		var new_gnd = groundTypes[i]
 		var gnd
 		gnd = new_gnd.instantiate()
-		lastGroundCalled = gnd
 		gnd.position = Vector2i(i*(screenSize.x*2),1100)
 		add_child(gnd)
 		groundInstances.append(gnd)
 		
 func _move_ground(ground):
 	ground.position.x -= SPEED
+	if ground.position.x < -4000:
+		ground.position.x = -4000
 
 
 func _swap_ground_instances():
