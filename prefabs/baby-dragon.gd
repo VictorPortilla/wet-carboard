2b2696f95e654190e4e754778da83636ad337396
coolestKitten <sepcaro2@gmail.com>
2024-08-18 01:12:39 -0600

    added logic to pick between middle and bottom lanes

--- prefabs/baby-dragon.gd
+++ prefabs/baby-dragon.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
 enum movement_phase {PHASE_ONE, PHASE_TWO, PHASE_THREE}
+var enemy_lanes : Array = [450, 800]
 var current_phase = movement_phase.PHASE_ONE
 var velocity : float = 650.0
-var vert_velocity : float = 27.0
+var vert_velocity : float = 30.0
+var descent_limit
+
+func _ready():
+	descent_limit = enemy_lanes[randi() % enemy_lanes.size()]
 
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -23,9 +28,10 @@ func _phase_one(delta):
 	
 	
 func _phase_two(delta):
+	print("Selected Element: ", descent_limit)
 	position.x += -velocity * 2 * delta
 	position.y -= -sqrt(((-100 * (position.x - 1350)) / 3)) * vert_velocity * delta
-	if position.y > 800:
+	if position.y > descent_limit:
 		current_phase = movement_phase.PHASE_THREE
 	
 	
