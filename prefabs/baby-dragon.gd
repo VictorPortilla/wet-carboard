d20a1cac83ebc5984175010eeddb3777db41f568
coolestKitten <sepcaro2@gmail.com>
2024-08-18 00:39:02 -0600

    added a curving motion to the baby dragon's descent

--- prefabs/baby-dragon.gd
+++ prefabs/baby-dragon.gd
@@ -3,12 +3,7 @@ extends Node2D
 enum movement_phase {PHASE_ONE, PHASE_TWO, PHASE_THREE}
 var current_phase = movement_phase.PHASE_ONE
 var velocity : float = 650.0
-var vert_velocity : float = 30.0
-
-## Called when the node enters the scene tree for the first time.
-#func _ready():
-	#pass # Replace with function body.
-
+var vert_velocity : float = 27.0
 
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -23,17 +18,18 @@ func _process(delta):
 func _phase_one(delta):
 	position.x += velocity * delta
 	if position.x > 1350:
+		
 		current_phase = movement_phase.PHASE_TWO
 	
 	
 func _phase_two(delta):
-	position.y += pow(vert_velocity, 2) * delta
-	position.x += -200 * delta
+	position.x += -velocity * 2 * delta
+	position.y -= -sqrt(((-100 * (position.x - 1350)) / 3)) * vert_velocity * delta
 	if position.y > 800:
 		current_phase = movement_phase.PHASE_THREE
 	
 	
 func _phase_three(delta):
-	position.x -= velocity * delta
+	position.x -= velocity * delta * 3.1
 	if position.x < -300:
 		queue_free()
